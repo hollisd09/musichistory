3467113e54b47deb880bd697d39ba85405c7e35c
Danielle Hollis <hollisd09@gmail.com>
2015-10-14 09:37:16 -0500

    Added new files

--- version2/songs.js
+++ version2/songs.js
@@ -1,11 +1,18 @@
-// Use JavaScript arrays, loops, and innerHTML to show the music you love.
+// // Use JavaScript arrays, loops, and innerHTML to show the music you love.
 
-// Students must use JavaScript to create a list of songs in the index.html file for their Music History project. Have them download the songs.js file, which contains an array of strings with song information.
+// // Students must use JavaScript to create a list of songs in the index.html file for their Music History project. Have them download the songs.js file, which contains an array of strings with song information.
 
-// Each student must add one song to the beginning and the end of the array.
-// Loop over the array and remove any words or characters that obviously don't belong.
-// Students must find and replace the > character in each item with a - character.
-// Must add each string to the DOM in index.html in the main content area.
+// // Each student must add one song to the beginning and the end of the array.
+// // Loop over the array and remove any words or characters that obviously don't belong.
+// // Students must find and replace the > character in each item with a - character.
+// // Must add each string to the DOM in index.html in the main content area.
+
+
+var songsElement = document.getElementById("right-bar");
+var output = "";
+var string = ("songs");
+var newCharacter = string.replace(/>/g, '-');
+
 
 
 var songs = [];
@@ -16,14 +23,21 @@ songs[songs.length] = "Leaf Off > by Jose Gonzalez on the album Vestiges an&d Cl
 songs[songs.length] = "Eyes Wide Ope(n > by Gotye on the album Making Mirrors";
 songs[songs.length] = "Love Is a Losing Game > by Amy Winehouse o*n the album Back to Black";
 
+
 songs.push("Need Your Love > by The Temper Trap on the album The Temper Trap");
 songs.unshift("Windows Are Rolled Down > by Amos Lee on the album Mission Bell");
 
+console.log(songs, "songs.length");
+
 for (var i = 0; i < songs.length; i++) {
+    var currentSong = songs[i];
 
+    currentSong = currentSong.replace(/[^a-z A-Z 0-9 \>]+/g, "").replace(">", "-");
+    console.log("currentSong", currentSong);
+
+    output = output + "<div class='song-list-item'>" + currentSong + "</div>";
 }
 
-var firstChar = songs.indexOf(">");
-if (firstChar !== -1) {
-	songs = songs.slice (0, firstChar) + "-";
-}
+console.log("output", output);
+
+songsElement.innerHTML = output;
